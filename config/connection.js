a8c0a2d5accb0411cb62ef6a312fd423aefb7d8a
dimitar-paskov <dimitarp2002@abv.bg>
2025-12-10 15:05:37 +0200

    test

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ const { getSecret } = require("./keyvault");
 async function putKeyVaultSecretInEnvVar() {
 
     const secretName = process.env.KEY_VAULT_SECRET_NAME_DATABASE_URL;
-    const keyVaultName = process.env.KEY_VAULT_NAME;
+    const keyVaultName = process.env.KEY_VAULT_NAME; 
 
     console.log(secretName);
     console.log(keyVaultName);
